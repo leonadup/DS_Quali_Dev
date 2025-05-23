8e122fd46774d521faa36cc01eb8f4f56265366c
leonadup <leona.d@icloud.com>
2025-05-23 10:27:51 +0200

    Initial commit

--- src/Statistique.java
+++ src/Statistique.java
@@ -1,2 +1,8 @@
 public class Statistique {
+    Calculatrice calculatrice = new Calculatrice();
+    public float moyenne(float x, float y){
+        return calculatrice.div(
+                calculatrice.add(x, y),
+                2);
+    }
 }
